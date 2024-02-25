17aa6d01063217a2bb981e9a2f9f007fd8853be5
Karol <karolcwiklik@o2.pl>
2024-02-25 13:04:06 +0100

    complete offer blog and cta sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,40 @@
 addEventListener('DOMContentLoaded', () => {
-
 	const nav = document.querySelector('.JSnav')
-	const navBtn = document.querySelector('.JSnavBtn')
+	if (nav) {
+		const navBtn = document.querySelector('.JSnavBtn')
+		window.addEventListener('scroll', () => {
+			if (window.scrollY > 450) {
+				nav.classList.add('nav--short')
+				nav.classList.add('nav--shadow')
+				navBtn.classList.add('btn--medium')
+			} else {
+				nav.classList.remove('nav--short')
+				navBtn.classList.remove('btn--medium')
+				nav.classList.remove('nav--shadow')
+			}
 
-	window.addEventListener('scroll', () => {
-		if (window.scrollY > 450) {
-			nav.classList.add('nav--short')
-			navBtn.classList.add('btn--medium')
-		} else {
-			nav.classList.remove('nav--short')
-			navBtn.classList.remove('btn--medium')
-		}
-	})
-
-
-
-
-
-
-
-
-
-
-
+			if (window.scrollY > 1) {
+				nav.classList.add('nav--shadow')
+			} else {
+				nav.classList.remove('nav--shadow')
+			}
+		})
+	}
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+   
+	//end
 })
